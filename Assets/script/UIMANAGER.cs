5f0ca31230fc3034125fc94b6c796e88e158da39
nacho883 <bourdieu.ignacio0@gmail.com>
2025-06-05 12:00:23 -0300

    ponele que esta hecho

---
+++ Assets/script/UIMANAGER.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIMANAGER : MonoBehaviour
+{
+    public TextMeshProUGUI txtMoney;
+    void Start()
+    {
+        
+    }
+
+   public void UpdateMoneyTxt(string moneyAmount)
+   {
+    txtMoney.text = moneyAmount;
+   }
+}
--- Assets/script/mercanciaScript.cs
+++ Assets/script/mercanciaScript.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mercanciaScript : MonoBehaviour
-{
-    private void OnCollisionEnter(Collision collision)
-        
-    {
-         if(collision.gameObject.name == "Player")
-       Destroy(gameObject);
+public class mercanciaScript : MonoBehaviour{
+  
+  public moneyManager moneymanager;
+  public float cost;
+  void start()
+  {
+    moneymanager = FindObjectOfType<moneyManager>();
+  }
+
+  void OnCollisionEnter(Collision collision){
+      if(collision.gameObject.name == "Player"){
+        moneymanager.updateMoney(cost);
+        Destroy(gameObject);
     }
+  }
 }
